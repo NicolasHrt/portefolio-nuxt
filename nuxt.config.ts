6208f7b9cea92c7f9d22d9afb07db71a5278c22a
Nico <72673830+NicolasHrt@users.noreply.github.com>
2024-05-07 16:09:46 +0200

    add nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,6 @@
 export default defineNuxtConfig({
     devtools: {enabled: true},
     extends: ['@nuxt/ui-pro'],
-    modules: ["@nuxt/ui", '@vueuse/motion/nuxt', "@nuxt/content", "@nuxt/image"],
-    runtimeConfig: {
-
-
-        // Public keys that are exposed to the client
-        public: {
-            email: 'harternicolas134@gmail.com'
-        }
-    }
+    modules: ["@nuxt/ui", '@vueuse/motion/nuxt', "@nuxt/content", "@nuxt/image", '@nuxthq/studio'],
+    runtimeConfig: {}
 })
