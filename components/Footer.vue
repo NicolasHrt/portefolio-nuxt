1f69ea050c0ef32449139012f6eddddad5a6665d
Nico <72673830+NicolasHrt@users.noreply.github.com>
2025-11-22 21:57:40 +0100

    update english

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,6 @@
       </p>
     </template>
     <template #right>
-      <UButton icon="i-simple-icons-x" color="gray" variant="ghost" to="https://x.com/nicolasharter_" target="_blank" />
       <UButton icon="i-simple-icons-linkedin" color="gray" variant="ghost"
         to="https://www.linkedin.com/in/nicolas-harter" target="_blank" />
       <UButton icon="i-simple-icons-github" color="gray" variant="ghost" to="https://github.com/NicolasHrt"
